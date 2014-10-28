96294328bcb1f86819ae17b2ec3f97c4ada045d1
AldoPadilla <ap1@icsart.com>
2014-10-28 19:22:39 -0400

    TK Assignment - Initial

---
+++ On_Off_Switch/TKButtonStatesBlinksDades/TKButtonStatesBlinksDades.ino
@@ -0,0 +1,65 @@
+/*
+  Test the different Button methods: pressed, released, held
+  and getSwitch. 
+  
+  modified in by Matteo Loglio on 6/7/13
+*/
+
+#include <TinkerKit.h>
+
+TKButton btn(I0);
+TKLed basic(O0);
+TKLed ratchet(O1);
+TKLed patricia(O2);
+
+int bright = 0;
+int maxBright = 1023;
+int speed = 40;
+int direction = 1;
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  if(btn.pressed()) {      
+      Serial.println("pressed");
+    basic.on();     // turn LED on 
+  } else {
+    basic.off();    // turn LED off
+  }
+  
+  
+  if(btn.held()) {      
+      Serial.println("held");
+     tina();     // turn LED on 
+  } 
+    
+    
+    
+    
+  if(btn.released()) {
+        
+      Serial.println("released");
+    patricia.on();     // turn LED on 
+  } else {
+    patricia.off();    // turn LED off
+  
+  }
+
+  delay(50);  
+}
+
+void tina(){
+    ratchet.brightness(bright);
+  bright += (speed * direction);
+  
+  if(bright > maxBright || bright < 0){
+    direction *= -1;
+  } 
+}
+
+
+
